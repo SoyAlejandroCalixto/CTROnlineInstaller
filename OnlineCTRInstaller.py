3c4d4911d39d725e8425796c760b7447a7e64d16
SoyAlejandroCalixto <SoyAlejandroCalixto@gmail.com>
2024-07-23 15:33:29 +0100

    download percentage and fixes

--- OnlineCTRInstaller.py
+++ OnlineCTRInstaller.py
@@ -7,27 +7,47 @@ from os import path
 import py7zr
 import zipfile
 
-CWD = os.getcwd()
-INSTALL_PATH = path.join(CWD, 'OnlineCTR')
+INSTALL_PATH = path.join(os.getcwd(), 'OnlineCTR')
+
+def download_file(url, path, output_object):
+    message = output_object['text']
+
+    res = requests.get(url, stream=True)
+
+    # Obtén el tamaño total del archivo
+    total_size = int(res.headers.get('content-length', 0))
+
+    # Inicializa una variable para llevar la cuenta de los datos descargados hasta ahora
+    downloaded_size = 0
+
+    with open(path, 'wb') as file:
+        for chunk in res.iter_content(chunk_size=1024):
+            if chunk:
+                downloaded_size += len(chunk)
+
+                percent = downloaded_size / total_size * 100
+
+                output_object.config(text=f'{message} ({int(percent)}%)')
+
+                file.write(chunk)
 
 def install(framerate, output, fps30button, fps60button):
     try:
         fps30button.config(state=tk.DISABLED)
         fps60button.config(state=tk.DISABLED)
 
+        # rom verification and download
+
         ROM_PATH = path.join(INSTALL_PATH, 'ctr.bin')
+
         if not path.exists(ROM_PATH):
             output.config(text=f'No se encontró la ROM del CTR. Descargándola...')
-            response = requests.get('https://ia601900.us.archive.org/6/items/ctr-crash-team-racing-usa-1999-ps1/CTR%20-%20Crash%20Team%20Racing%20%28USA%29.7z')
 
             if not path.exists(INSTALL_PATH):
                 os.mkdir(INSTALL_PATH)
 
             ZIPPED_ROM_PATH = path.join(INSTALL_PATH, 'ctr.7z')
-
-            file = open(ZIPPED_ROM_PATH, 'wb')
-            file.write(response.content)
-            file.close()
+            download_file('https://drive.usercontent.google.com/download?id=1HePT1AUTv-UUYhyl3n12xaVRxiZqgKzM&export=download&authuser=0&confirm=t&uuid=d566728c-bcfb-4fd9-bd5e-fa28eda20cc5&at=APZUnTXueQvUe7SoI5QAOZwvZ_uL%3A1721740758809', ZIPPED_ROM_PATH, output)
 
             output.config(text=f'Extrayendo la ROM comprimida...')
 
@@ -36,19 +56,15 @@ def install(framerate, output, fps30button, fps60button):
             archive.close()
 
             os.remove(path.join(INSTALL_PATH, 'ctr.7z'))
-            os.remove(path.join(INSTALL_PATH, 'CTR - Crash Team Racing (USA).cue'))
-            os.rename(path.join(INSTALL_PATH, 'CTR - Crash Team Racing (USA).bin'), path.join(INSTALL_PATH, 'ctr.bin'))
         else:
             output.config(text=f'Ya tiene la ROM del CTR. Se omitirá su instalación...')
 
+        # downloading patch and applying it to the rom
+
         output.config(text=f'Descargando el parche...')
-        response = requests.get(f'https://online-ctr.com/wp-content/uploads/onlinectr_patches/ctr-u_Online{framerate}.xdelta')
 
         PATCH_PATH = path.join(INSTALL_PATH, f'ctr-u_Online{framerate}.xdelta')
-
-        file = open(PATCH_PATH, 'wb')
-        file.write(response.content)
-        file.close()
+        download_file(f'https://online-ctr.com/wp-content/uploads/onlinectr_patches/ctr-u_Online{framerate}.xdelta', PATCH_PATH, output)
 
         if path.exists(path.join(INSTALL_PATH, 'OnlineCTR.bin')):
             os.remove(path.join(INSTALL_PATH, 'OnlineCTR.bin'))
@@ -58,33 +74,35 @@ def install(framerate, output, fps30button, fps60button):
 
         os.remove(path.join(INSTALL_PATH, path.join(INSTALL_PATH, f'ctr-u_Online{framerate}.xdelta')))
 
-        output.config(text=f'Descargando el cliente...')
-        CLIENT_PATH = path.join(INSTALL_PATH, 'client.zip')
+        # downloading client
 
-        response = requests.get('https://online-ctr.com/wp-content/uploads/onlinectr_patches/client.zip')
-        
-        file = open(CLIENT_PATH, 'wb')
-        file.write(response.content)
-        file.close()
+        output.config(text=f'Descargando el cliente...')
+
+        CLIENT_PATH = path.join(INSTALL_PATH, 'client.zip')
+        download_file('https://online-ctr.com/wp-content/uploads/onlinectr_patches/client.zip', CLIENT_PATH, output)
 
         output.config(text=f'Extrayendo el cliente comprimido...')
+
         archive = zipfile.ZipFile(CLIENT_PATH, 'r')
         archive.extractall(path=INSTALL_PATH)
         archive.close()
 
         os.remove(CLIENT_PATH)
 
+        # downloading config file
+
         output.config(text=f'Descargando la configuración del juego...')
-        CONFIG_PATH = path.expanduser(path.join('~', 'Documents', 'DuckStation', 'gamesettings', 'SCUS-94426.ini'))
+
+        GAMESETTINGS_PATH = path.expanduser(path.join('~', 'Documents', 'DuckStation', 'gamesettings'))
+        CONFIG_PATH = path.join(GAMESETTINGS_PATH, 'SCUS-94426.ini')
+
+        if not path.exists(GAMESETTINGS_PATH):
+            os.makedirs(GAMESETTINGS_PATH)
 
         if path.exists(CONFIG_PATH):
             os.remove(CONFIG_PATH)
 
-        response = requests.get('https://online-ctr.com/wp-content/uploads/onlinectr_patches/SCUS-94426.ini')
-
-        file = open(CONFIG_PATH, 'wb')
-        file.write(response.content)
-        file.close()
+        download_file('https://online-ctr.com/wp-content/uploads/onlinectr_patches/SCUS-94426.ini', CONFIG_PATH, output)
 
         output.config(text=f'OnlineCTR se ha instalado correctamente. Cierra esta ventana para salir.')
     except Exception as e:
